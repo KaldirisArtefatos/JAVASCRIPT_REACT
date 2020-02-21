803d0eec0bb5ca684ef2e1bbbb8767aa4cdcf82d
unknown <treinamento@administrador4.DTRAINNING.local>
2020-02-20 21:46:01 -0300

    Instalou a Mensagem com Bootstrap

--- trainning_react/src/pages/main/index.js
+++ trainning_react/src/pages/main/index.js
@@ -1,18 +1,27 @@
+
 import React, { Component } from 'react';
 import api from '../../services/api';
 import './styles.css';
+import _ from 'lodash';
+ 
  
 class Main extends Component {
     
- 
     // PULO DO GATO do React
+    // Manipula a mensagem...
     state = {
-        produtos: []
+        produtos: [],
+        showMessage: false,
+        message: ''
     }
  
     componentDidMount(){
         // Iniciando por aqui
-        this.loadProdutos();
+        this.loadProdutos();     
+    }
+ 
+    closeMessage = () => {
+        this.setState({showMessage: false, message: '', cssClass: ''});
     }
  
     loadProdutos = async () => {
@@ -40,7 +49,10 @@ class Main extends Component {
         this.setState({ produtos: produtos.data});
     }
  
- 
+    // Forma 1
+    //excluir = async (event) => {
+    
+    // Forma 2
     excluir = async (idProduto) => {
         //console.log(event.target.getAttribute('data-id'));
         //const idProduto = event.target.getAttribute('data-id');
@@ -52,60 +64,55 @@ class Main extends Component {
  
         const response = await api.delete('produtos/' + idProduto, {
             headers: headers            
-        }).then(resp =>{
-            console.log('Produto excluido com sucesso');
- 
-            // TODO: Splice no array
-            // this.setState({produtos: this.state.produtos})
-            window.location.reload();
-        });
- 
-        
-    }  
-
- 
- 
-
+        }).then(resp =>{            
+            this.setState({showMessage: true, message: 'Produto excluido com sucesso', cssClass: 'success'});
+            this.setState({produtos: _.filter(this.state.produtos, function(p){ return p.id != idProduto})});
+        });        
+    }     
  
     render() { 
         return ( 
-            <table class="table">
-                <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Nome</th>
-                    <th scope="col">Categoria</th>
-                    <th scope="col">Valor</th>
-                    <th scope="col">Ações</th>
-                </tr>
-                </thead>
-                <tbody>                
-                    {this.state.produtos.map(produto =>(
-                        <tr key={produto.id}>
-                            <th scope="row">{produto.id}</th>
-                            <td>{produto.nome}</td>
-                            <td>{produto.categoria}</td>
-                            <td>{produto.valor}</td>
-                            <td>
-                                {/* <button data-id={produto.id} onClick={e => this.excluir(e)} class="btn btn-danger btn-sm" >Excluir</button> */}
-
-                                <button 
-                                        onClick={e => 
-                                            window.confirm("Apagar Mesmo ?") &&
-                                            this.excluir(produto.id)
-                                        } 
-                                        class="btn btn-danger btn-sm" >Excluir</button>
-
-
-                            </td>
-                        </tr>              
-                    ))}
-                </tbody>
-            </table>
+            <div>
+                { this.state.showMessage ? 
+                    <div class="alert alert-success" role="alert">
+                            {this.state.message}
+                            <span onClick={this.closeMessage} class="fechar">X</span>
+                    </div> : ''
+                }
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Categoria</th>
+                        <th scope="col">Valor</th>
+                        <th scope="col">Ações</th>
+                    </tr>
+                    </thead>
+                    <tbody>                
+                        {this.state.produtos.map(produto =>(
+                            <tr key={produto.id}>
+                                <th scope="row">{produto.id}</th>
+                                <td>{produto.nome}</td>
+                                <td>{produto.categoria}</td>
+                                <td>{produto.valor}</td>
+                                <td>
+                                    {/* <button data-id={produto.id} onClick={e => this.excluir(e)} class="btn btn-danger btn-sm" >Excluir</button> */}
+                                    <button onClick={e => 
+                                                window.confirm("Você realmente deseja excluir o produto?") &&
+                                                this.excluir(produto.id)
+                                            } 
+                                            class="btn btn-danger btn-sm" 
+                                    >Excluir</button>
+                                </td>
+                            </tr>              
+                        ))}
+                    </tbody>
+                </table>
+            </div>
          );
     }
 }
  
 export default Main;
- 
  
